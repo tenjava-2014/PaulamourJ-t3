4000c11a82f5f274d087c9f3dc5ee4408579497a
Paulamourj <p-aj@live.fr>
2014-07-12 17:00:04 +0200

    Theme choice

--- src/main/java/com/tenjava/entries/PaulamourJ/t3/TenJava.java
+++ src/main/java/com/tenjava/entries/PaulamourJ/t3/TenJava.java
@@ -1,5 +1,12 @@
 package com.tenjava.entries.PaulamourJ.t3;
 
+import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class TenJava extends JavaPlugin {}
+public class TenJava extends JavaPlugin implements Listener {
+	
+	public void onEnable(){
+		
+	}
+	
+}
